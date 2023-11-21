eca4107d0e13adf887d548eaaaf66b006c344188
Rune Espenakk <143501123+pandolini@users.noreply.github.com>
2023-11-21 15:04:37 +0100

    Implemented the previewBlock and changed background colour to black

--- bin/main.cpp
+++ bin/main.cpp
@@ -10,11 +10,11 @@ using namespace tetris;
 
 int main() {
     Canvas canvas("Tetris");
-    WindowSize tetrisSize{500, 800};
+    WindowSize tetrisSize{650, 800};
     WindowSize glrSize{800, 800};
     canvas.setSize(tetrisSize);
     GLRenderer glr(glrSize);
-    glr.setClearColor(Color::pink);
+    glr.setClearColor(Color::black);
     auto scene = Scene::create();
     auto camera = OrthographicCamera::create();
     camera->position.z = 5;
@@ -28,12 +28,18 @@ int main() {
     Board board = Board();
     Random random = Random();
     std::vector<Block> blocks = {T_Tetromino(), S_Tetromino(), Z_Tetromino(), L_Tetromino(), J_Tetromino(), I_Tetromino(), O_Tetromino()};
-    Block currentBlock = blocks[random.getType()];
-    Block nextBlock = blocks[random.getType()];
+    int currentType = random.getType();
+    int nextType = random.getType();
+    Block currentBlock = blocks[currentType];
+    Block nextBlock = blocks[nextType];
     std::shared_ptr<threepp::Group> gridGroup = board.drawGrid();
     scene->add(gridGroup);
     std::shared_ptr<threepp::Group> blockGroup = currentBlock.draw();
     scene->add(blockGroup);
+    nextBlock.columnOffset = -4;
+    nextBlock.rowOffset = 1;
+    std::shared_ptr<threepp::Group> nextBlockGroup = nextBlock.draw();
+    scene->add(nextBlockGroup);
 
 
     Clock clock;
@@ -92,9 +98,11 @@ int main() {
                 drop = false;
                 currentBlock.rowOffset = -1;
                 currentBlock.columnOffset = 4;
+
                 blockGroup->clear();
-                currentBlock = nextBlock;
-                nextBlock = blocks[random.getType()];
+                currentType = nextType;
+                nextType = random.getType();
+                currentBlock = blocks[currentType];
                 blockGroup = currentBlock.draw();
                 scene->add(blockGroup);
 
@@ -102,6 +110,13 @@ int main() {
                 gridGroup->clear();
                 gridGroup = board.drawGrid();
                 scene->add(gridGroup);
+
+                nextBlockGroup->clear();
+                nextBlock = blocks[nextType];
+                nextBlock.rowOffset = 1;
+                nextBlock.columnOffset = -4;
+                nextBlockGroup = nextBlock.draw();
+                scene->add(nextBlockGroup);
             }
         }
     });
