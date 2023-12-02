9662fd145f8cbc4be3ddbd22a70e2c2cfdf3ee1c
Rune <143501123+pandolini@users.noreply.github.com>
2023-12-02 23:14:42 +0100

    updated vcpkg version fix
    Signed-off-by: Rune <143501123+pandolini@users.noreply.github.com>

--- tests/BlockType.cpp
+++ tests/BlockType.cpp
@@ -1,6 +1,22 @@
 
+#include "Block.hpp"
+#include "Tetrominos.hpp"
 #include <catch2/catch_test_macros.hpp>
 
+using namespace tetris;
 TEST_CASE("test tetromino types") {
-    CHECK(0 == 0);
+    T_Tetromino t;
+    S_Tetromino s;
+    Z_Tetromino z;
+    L_Tetromino l;
+    J_Tetromino j;
+    I_Tetromino i;
+    O_Tetromino o;
+    CHECK(t.type == 0);
+    CHECK(s.type == 1);
+    CHECK(z.type == 2);
+    CHECK(l.type == 3);
+    CHECK(j.type == 4);
+    CHECK(i.type == 5);
+    CHECK(o.type == 6);
 }
