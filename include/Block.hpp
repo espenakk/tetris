0d833db1ac35b50c75bda36edba5dd12f8dd5c4c
Rune <143501123+pandolini@users.noreply.github.com>
2023-12-07 13:20:29 +0100

    Cleaned up the block class by removing the rendering methods. ++ Removed redundant includes.
    Signed-off-by: Rune <143501123+pandolini@users.noreply.github.com>

--- include/Block.hpp
+++ include/Block.hpp
@@ -3,8 +3,8 @@
 #define TETRIS_BLOCK_HPP
 #include "Position.hpp"
 #include <map>
-#include <threepp/threepp.hpp>
 #include <vector>
+
 namespace tetris {
 
     class Block {
@@ -12,7 +12,6 @@ namespace tetris {
         Block();
         int type;
         std::map<int, std::vector<Position>> cells;
-        std::shared_ptr<threepp::Group> draw();
         void move(int row, int column);
         std::vector<Position> peak(int row, int column, bool rotate);
         void rotate();
@@ -24,7 +23,6 @@ namespace tetris {
     private:
         int rotationState;
         std::vector<Position> blockPositions(int x, int y, int rot);
-        std::vector<threepp::Color> colours;
     };
 
 }// namespace tetris
--- src/Block.cpp
+++ src/Block.cpp
@@ -1,24 +1,13 @@
 
 #include "Block.hpp"
-#include "Input.hpp"
-#include "Render.hpp"
+
 namespace tetris {
     Block::Block() {
         rotationState = 0;
         rowOffset = 0;
         columnOffset = 0;
-        colours = {threepp::Color::blueviolet, threepp::Color::limegreen, threepp::Color::red, threepp::Color::darkorange, threepp::Color::deepskyblue, threepp::Color::cyan, threepp::Color::yellow};
     }
-    std::shared_ptr<threepp::Group> Block::draw() {
-        Render render(0.9, 0.9);
-        std::shared_ptr<threepp::Group> blockGroup = threepp::Group::create();
-        blockGroup->clear();
-        std::vector<Position> tiles = blockPositions();
-        for (Position item : tiles) {
-            blockGroup->add(render.createBox({(float) (item.column), (float) (item.row), 0}, colours[type]));
-        }
-        return blockGroup;
-    }
+
     void Block::move(int row, int column) {
         rowOffset += row;
         columnOffset += column;
