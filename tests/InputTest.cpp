40d5583a9840328d4cce2bd700be95972c18c8d0
Rune <143501123+pandolini@users.noreply.github.com>
2023-12-10 18:15:48 +0100

    Started making unit tests for the project.
    Signed-off-by: Rune <143501123+pandolini@users.noreply.github.com>

---
+++ tests/InputTest.cpp
@@ -0,0 +1,8 @@
+
+#include "Input.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("stupid pointless test") {
+    CHECK(1 == 1);
+    CHECK(2 + 2 == 4);
+}
--- tests/TetrominosTest.cpp
+++ tests/TetrominosTest.cpp
@@ -1,9 +1,9 @@
 
-#include "Block.hpp"
 #include "Tetrominos.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 using namespace tetris;
+
 TEST_CASE("test tetromino types") {
     T_Tetromino t;
     S_Tetromino s;
@@ -12,11 +12,11 @@ TEST_CASE("test tetromino types") {
     J_Tetromino j;
     I_Tetromino i;
     O_Tetromino o;
-    CHECK(t.type == 0);
-    CHECK(s.type == 1);
-    CHECK(z.type == 2);
-    CHECK(l.type == 3);
-    CHECK(j.type == 4);
-    CHECK(i.type == 5);
-    CHECK(o.type == 6);
+    CHECK(t.type == 1);
+    CHECK(s.type == 2);
+    CHECK(z.type == 3);
+    CHECK(l.type == 4);
+    CHECK(j.type == 5);
+    CHECK(i.type == 6);
+    CHECK(o.type == 7);
 }
